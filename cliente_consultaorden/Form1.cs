fe5b82bf3b3d29545af01d3f0b02f84a51ecec8e
khryztiam <khryztiam@users.noreply.github.com>
2020-08-22 20:10:10 -0600

    pruebas

---
+++ cliente_consultaorden/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cliente_consultaorden
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebService1SoapClient qry = new ServiceReference1.WebService1SoapClient();
+
+            System.Data.DataSet ds = new DataSet();
+
+            ds = qry.ListOrders();
+
+            this.dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
